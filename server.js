d0b55d8dc85f0a8f7d437d32b6f7dc369a818831
Jason <haloos@me.com>
2019-09-25 09:49:01 -0400

    added bodyParser

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express'); 
+const bodyParser = require('body-parser'); 
 
+app.use(bodyParser.json());
 const app = express();  
 
 const database = {
   users: [
     {
-      
+      id: '123', 
+      name: 'John', 
+      email: 'john@gmail.com', 
+      password: 'cookies', 
+      entries: 0, 
+      joined: new Date()
+    }, 
+    {
+      id: '124', 
+      name: 'Sally', 
+      email: 'sally@gmail.com', 
+      password: 'bananas', 
+      entries: 0, 
+      joined: new Date()
     }
   ]
 }
