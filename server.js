db73dc3b733176033597fcf969ad82a7b0584683
Jason <haloos@me.com>
2019-09-27 13:16:19 -0400

    created cont cprs

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express'); 
 const bodyParser = require('body-parser'); 
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt') 
+const cors = require('cors');
 
 const app = express();  
 
