32e6b8cdf647819c358a4796683d11ff4e898073
Jason <haloos@me.com>
2019-10-10 17:21:59 -0400

    added knex

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express'); 
 const bodyParser = require('body-parser'); 
 const bcrypt = require('bcrypt-nodejs'); 
-const cors = require('cors');
+const cors = require('cors'); 
+const knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host : '127.0.0.1',
+    user : 'your_database_user',
+    password : 'your_database_password',
+    database : 'myapp_test'
+  }
+});
 
 const app = express();  
 
