778d9a7d2c678adef555d75e7459b6b69cb2d14f
Jason <haloos@me.com>
2019-10-11 12:57:33 -0400

    added postgres

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors'); 
 const knex = require('knex')
   
-  const database = knex({
+  const postgres = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
@@ -14,7 +14,7 @@ const knex = require('knex')
   }
 }); 
 
-knex.select('*').from('users');
+postgres.select('*').from('users');
 
 const app = express();  
 
