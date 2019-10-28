35bb44b8ac98174e21677e31b940569ad2ac9a2c
Jason Paredes <haloos@me.com>
2019-10-28 19:39:57 -0400

    added res.status

--- server.js
+++ server.js
@@ -74,10 +74,14 @@ app.get('/profile/:id', (req,res) => {
   const { id } = req.params;  
   db.select('*').from('users').where({id})
     .then(user => { 
-    console.log(user);
-    res.json(user[0])
+    if (user.length) {
+      res.json(user[0])
+    } else {
+      res.status(400).json('Not found')
+    }
+    
   }) 
-  .catch(err => res.status(400).json('Not found'))
+  .catch(err => res.status(400).json('error getting user'))
 })
 
 app.listen(3000, ()=> {
