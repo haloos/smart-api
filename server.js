1a401936183f8fc8bbc6aa650a30529eed27cf19
Jason Paredes <haloos@me.com>
2019-10-28 19:18:46 -0400

    delated databse

--- server.js
+++ server.js
@@ -72,13 +72,7 @@ app.post('/register', (req,res) => {
 
 app.get('/profile/:id', (req,res) => {
   const { id } = req.params;  
-  database.users.forEach(user => {
-    if (user.id === id) {
-      res.json(user); 
-    } else {
-      res.status(404).json('no such user'); 
-    }
-  }) 
+  
 })
 
 app.listen(3000, ()=> {
