a5d0f4f9c8f7127c2e80615e5e26a9a0941c0c5a
Jason Paredes <haloos@me.com>
2019-11-01 22:00:40 -0400

    added else statement

--- server.js
+++ server.js
@@ -51,6 +51,7 @@ app.post('/signin', (req, res) => {
     .where('email', '=', req.body.email)
     .then(data => {
      const isValid = bcrypt.compareSync(req.body.password, data[0].hash); 
+     console.log(isValid); 
      if (isValid) { 
        return db.select('*').from('users')  
         .where('email', '=', req.body.email) 
@@ -59,7 +60,10 @@ app.post('/signin', (req, res) => {
           res.json(user[0]) 
         })
         .catch(err => res.status(400).json('unable to get user'))
+     } else {
+      res.status(400).json('wrong credentials')
      }
+     
     }) 
         .catch(err => res.status(400).json('wrong credentials'))
 })  
