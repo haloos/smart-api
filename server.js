b3e01c8e5358d11ffe59e7ca6ebd4f58029f23e7
Jason <haloos@me.com>
2019-11-05 11:13:59 -0500

    created signin.handleSignin

--- server.js
+++ server.js
@@ -29,26 +29,9 @@ app.get('/', (req, res)=> {
  res.send(database.users); 
 })
 
-app.post('/signin', (req, res) => {
-  db.select('email', 'hash').from('login') 
-    .where('email', '=', req.body.email)
-    .then(data => {
-     const isValid = bcrypt.compareSync(req.body.password, data[0].hash); 
-     console.log(isValid); 
-     if (isValid) { 
-       return db.select('*').from('users')  
-        .where('email', '=', req.body.email) 
-        .then(user => { 
-          res.json(user[0]) 
-        })
-        .catch(err => res.status(400).json('unable to get user'))
-     } else {
-      res.status(400).json('wrong credentials')
-     }
-     
-    }) 
+app.post('/signin', (req, res,) => { signin.handleSignin(req, res, db, bcrypt)}) 
         .catch(err => res.status(400).json('wrong credentials'))
-})  
+  
 
 app.post('/register', (req,res) => { 
   const { email, name, password } = req.body; 
