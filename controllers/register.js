6117de387212b3054479ef09e23b32d128e64833
Jason <haloos@me.com>
2019-11-05 10:51:23 -0500

    added module.exports

--- controllers/register.js
+++ controllers/register.js
@@ -23,4 +23,8 @@
       .then(trx.commit) 
       .catch(trx.rollback)
   }) 
-  .catch(err => res.status(400).json('unable to register'))}
+  .catch(err => res.status(400).json('unable to register'))} 
+
+  module.exports = {
+    handleRegister: handleRegister
+  };
