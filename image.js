3c0af20377a57c7982a294907a54996b847f3032
Jason <haloos@me.com>
2019-11-05 11:56:47 -0500

    created module.exports

--- image.js
+++ image.js
@@ -1,4 +1,4 @@
-const handleImage = (req, res) => {
+const handleImage = (req, res, db) => {
   const { id } = req.body; 
    db('users').where('id', '=', id)
   .increment('entries', 1) 
@@ -7,4 +7,8 @@ const handleImage = (req, res) => {
     res.json(entries[0]);
   })  
 .catch(err => res.status(400).json('unable to get entries'))
-}
+}  
+
+module.exports = {
+  handleImage
+};
