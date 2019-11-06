e2151c9415d2c35b9cb1cac81df92276653e2777
Jason <haloos@me.com>
2019-11-06 10:41:27 -0500

    removed req.body

--- signin.js
+++ signin.js
@@ -4,9 +4,9 @@ const handleSignin = (req, res, db, bcrypt) => {
     return res.status(400).json('incorrect form submission'); 
   }
   db.select('email', 'hash').from('login') 
-    .where('email', '=', req.body.email)
+    .where('email', '=',email)
     .then(data => {
-     const isValid = bcrypt.compareSync(req.body.password, data[0].hash); 
+     const isValid = bcrypt.compareSync(password, data[0].hash); 
      console.log(isValid); 
      if (isValid) { 
        return db.select('*').from('users')  
